b886c49f0d1d0a487b56d72556b52f8d0594481d
heberibeiro98 <62151872+heberibeiro98@users.noreply.github.com>
2020-04-11 19:25:42 +0100

    Update ex1b.c

--- TP5/ex1b.c
+++ TP5/ex1b.c
@@ -13,15 +13,14 @@ int main(int argc, char *argv[])
   long counter = 0;
   int nvezes, i;
   pthread_t thread[3];
-  targ_t args[3], *aux;
+  targ_t args[3];
   sscanf(argv[1], "%d", &nvezes);
   for(i = 0; i < 3; i++)
   {
-    aux = &args[i];
-    aux->cnt = &counter;
-    aux->n = nvezes;
-    aux->id = i;
-    pthread_create(&(thread[i]), 0, sfun, aux);
+    (args+i)->cnt = &counter;
+    (args+i)->n = nvezes;
+    (args+i)->id = i;
+    pthread_create(&(thread[i]), 0, sfun, &(args[i]));
   }
   for(i = 0; i < 3; i++)
   {
